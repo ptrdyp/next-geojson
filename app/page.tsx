b7b87b12278bafaebb22527a4fbc73a262f018a5
Putri Alviany Dyah Prameswari <alviany.prameswari@gmail.com>
2025-03-03 13:26:08 +0700

    feat(map): show map with geojson and hover feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,113 @@
-import Image from "next/image";
+'use client'
+import bbox from '@turf/bbox';
+import React, { useEffect, useRef, useState } from 'react';
+import Map, { Layer, MapRef, Source, MapMouseEvent } from 'react-map-gl/mapbox';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [geoJsonData, setGeoJsonData] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const requestGeoJSON = async () => {
+    try {
+      const response = await fetch('/data/provinces.geojson');
+      if (!response.ok) {
+          throw new Error('Gagal mengambil data GeoJSON');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching GeoJSON:', error);
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const data = await requestGeoJSON();
+        setGeoJsonData(data);
+    };
+    fetchData();
+  }, []);
+
+  // zoom to bounds
+  const mapRef = useRef<MapRef | null>(null);
+  
+  const handleClick = (event: MapMouseEvent) => {
+    const feature = event.features?.[0];
+    
+    if(feature) {
+      const [minLng, minLat, maxLng, maxLat] = bbox(feature);
+
+    mapRef.current?.fitBounds(
+      [
+        [minLng, minLat],
+        [maxLng, maxLat]
+      ],
+      {padding: 40, duration: 1000}
+    );
+    }
+  }
+
+  // hover polygon
+  const [hoveredPolygon, setHoveredPolygon] = useState<any>(null)
+
+  const handleMouseMove = (event: MapMouseEvent) => {
+    const feature = event.features?.[0];
+    console.log(feature?.id)
+    
+    if(feature?.id !== undefined) {
+      setHoveredPolygon(feature.id);
+    } else {
+      setHoveredPolygon(null)
+    }
+  }
+
+  const handleMouseLeave = () => {
+    setHoveredPolygon(null)
+  }
+
+  return (
+    <div className='flex flex-col'>
+      <Map
+        ref={mapRef}
+        mapboxAccessToken="pk.eyJ1IjoicHRyZHlwIiwiYSI6ImNtN3NpN2hkaDByM2cya29pdXUydzAwbmQifQ.72ScIGq_viZAk6sKiF_OxA"
+        initialViewState={{
+          longitude: 117.3591791339498,
+          latitude: -2.3078566449132154,
+          zoom: 3.8
+        }}
+        style={{width: 900, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        interactiveLayerIds={['provinces-layer']}
+        onClick={handleClick}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        {geoJsonData && (
+          <Source type='geojson' data={geoJsonData}>
+            <Layer
+              id="provinces-layer"
+              type="fill"
+              paint={{
+                "fill-color": [
+                  "case",
+                  ["==", ["id"], hoveredPolygon ?? null], "#ffa200", "#0080ff"
+                ],
+                "fill-opacity": 0.2
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <Layer
+              id="provinces_line"
+              type="line"
+              paint={{
+                  "line-color": "#a83232",
+                  "line-width": 1,
+                  "line-opacity": 1
+              }}
+              layout={{ visibility: 'visible' }}
+            />
+          </Source>
+        )}
+      </Map>
     </div>
   );
 }
